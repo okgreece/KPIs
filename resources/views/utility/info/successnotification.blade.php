ccb3407939d70c5cc8b3aa67f9ec117b7c21bfb3
skarampatakis <s.karampatakis@gmail.com>
2017-06-23 13:47:08 +0300

    restructed resource folders

--- resources/views/utility/info/successnotification.blade.php
+++
@@ -1,6 +0,0 @@
-
-    <div class="alert alert-success alert-dismissible fade in">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        <h4><i class="icon fa fa-check"></i> Notification</h4>
-        <p>{{session()->pull('flash_message')}}</p>
-    </div>
